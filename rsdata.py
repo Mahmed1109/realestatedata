0c19c28e3a51151c58c85cad1d92e2978c25bbf6
Mahmed1109 <m.ahmed110900@gmail.com>
2025-07-02 11:07:38 +0100

    feature enginerring

--- rsdata.py
+++ rsdata.py
@@ -16,3 +16,20 @@ from sklearn.impute import SimpleImputer
 # Step 2: Load dataset (California Housing)
 url = 'https://raw.githubusercontent.com/ageron/handson-ml2/master/datasets/housing/housing.csv'
 df = pd.read_csv(url)
+
+
+print("Initial data shape:", df.shape)
+print(df.head())
+
+df['rooms_per_household'] = df['total_rooms'] / df['households']
+df['bedrooms_per_room'] = df['total_bedrooms'] / df['total_rooms']
+df['population_per_household'] = df['population'] / df['households']
+
+print("\nMissing values before imputation:")
+print(df.isnull().sum())
+
+imputer = SimpleImputer(strategy='median')
+df['total_bedrooms'] = imputer.fit_transform(df[['total_bedrooms']])
+
+print("\nMissing values after imputation:")
+print(df.isnull().sum())
